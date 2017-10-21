271c37830e7b3abd136d4c6befd91757cf5925f2
Dimitry <dimmpixeye@users.noreply.github.com>
2017-10-21 23:17:01 +0200

    Update IPoolable.cs

--- Assets/PoolManager/IPoolable.cs
+++ Assets/PoolManager/IPoolable.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+namespace Homebrew{
 public interface IPoolable
 {
     void OnSpawn();
     void OnDespawn();
     
+}
 }
